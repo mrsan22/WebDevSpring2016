1802d22f7f4b632bcab5ed102253ce6cabd6b182
Sanjiv Kumar <skumar@basistech.com>
2016-01-17 15:50:18 -0500

    Modified server.js to render 'index.html' as landing page, also created css and js folder, having empty style and js files.

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/', function (req, res){
-   res.send("Hi, This is the landing page for Web Development 2016!. Append the URL with '/hello' to see the " +
-       " 'Hello World' message.")
+   //res.send("Hi..I am Sanjiv, This is the landing page for Web Development 2016!. Append the URL with '/hello' to see the " +
+   //    " 'Hello World' message.")
+    res.sendfile('index.html');
 });
 app.get('/hello', function(req, res){
   res.send('Hello World!');
