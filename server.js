59fb2d4e10f3f2c0990ab7e133f9cb73282adb20
Sanjiv Kumar <skumar@basistech.com>
2016-01-19 23:20:56 -0500

    Added comments in server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+//Require express module to route to the given URL
 var express = require('express');
 var app = express();
+
+// With below syntax for ipaddress and port, the application can run both locally and on openshift.
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/', function (req, res){
    //res.send("Hi..I am Sanjiv, This is the landing page for Web Development 2016!. Append the URL with '/hello' to see the " +
    //    " 'Hello World' message.")
