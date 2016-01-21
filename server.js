bd479246f975d261ad78f889e3417d6f22090c73
Sanjiv Kumar <skumar@basistech.com>
2016-01-21 15:44:23 -0500

    Checking /public directory to work remotely

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get('/', function (req, res){
-   //res.send("Hi..I am Sanjiv, This is the landing page for Web Development 2016!. Append the URL with '/hello' to see the " +
-   //    " 'Hello World' message.")
-    res.sendfile('index.html');
-});
+//app.get('/', function (req, res){
+//   //res.send("Hi..I am Sanjiv, This is the landing page for Web Development 2016!. Append the URL with '/hello' to see the " +
+//   //    " 'Hello World' message.")
+//    res.sendfile('index.html');
+//});
 
 app.get('/hello', function(req, res){
   res.send('Hello World!');
