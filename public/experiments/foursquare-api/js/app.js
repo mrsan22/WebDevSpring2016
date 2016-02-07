88d5bc1e915ef1d4c91d0ed5d1d2516e9c68afa4
Sanjiv Kumar <skumar@basistech.com>
2016-02-06 19:20:27 -0500

    modified index.html and app.js under experiments/foursqr api to create a UI for searching the API

--- public/experiments/foursquare-api/js/app.js
+++ public/experiments/foursquare-api/js/app.js
@@ -1,13 +1,14 @@
 /**
  * Created by skumar on 2/5/16.
  //*/
-// IIFE (Immediately Invoked function expression) design pattern
+// IIFE (Immediately Invoked function expression) design pattern.
+// When init gets called, it's guaranteed that page has been loaded.
 
 (function (){
   $(init);
 
     function init(){
-        alert("hello from foursquare api")
-        
+        //alert("hello from foursquare api")
+
     }
 })();
