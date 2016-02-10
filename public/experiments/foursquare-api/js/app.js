3cca18e8f483e9b3bcfe2b81792bdcb93e9e8c19
Sanjiv Kumar <skumar@basistech.com>
2016-02-10 16:29:23 -0500

    Tested Yelp API

--- public/experiments/foursquare-api/js/app.js
+++ public/experiments/foursquare-api/js/app.js
@@ -7,49 +7,101 @@
   var $restNametxt;
   var $locationtxt;
   var $searchRestbtn;
-  var loc, food_item, lim;
+  //var loc, food_item, lim;
 
-  //Foursquare API details
-  var client_id = "NAI3NNBC3VMAH4J4QGIPU3GMUAAYFZLL4DKUHJIYTJE11T0S";
-  var client_secret = "MWQ5YCT0QGQMTIEZXCDEXB0FE1YKMC3JGIIJGW3HU3AJNNTR";
-  var food_id = "4d4b7105d754a06374d81259";
-  var apiUrl = "https://api.foursquare.com/v2/venues/search?client_id="+client_id+"&client_secret="+client_secret+"" +
-      "&v=20130815&near=loc&query=food_item&categoryId="+food_id+"&limit=Lim";
-
-    function init(){
-        //alert("hello from foursquare api")
-      $restNametxt = $("#restNametxt");
-      $locationtxt = $("#locationtxt");
-      $searchRestbtn = $("#searchRestbtn");
-
-      $searchRestbtn.click(searchFourSqrApi);
+  ////Foursquare API details
+  //var client_id = "NAI3NNBC3VMAH4J4QGIPU3GMUAAYFZLL4DKUHJIYTJE11T0S";
+  //var client_secret = "MWQ5YCT0QGQMTIEZXCDEXB0FE1YKMC3JGIIJGW3HU3AJNNTR";
+  //var food_id = "4d4b7105d754a06374d81259";
+  //var apiUrl = "https://api.foursquare.com/v2/venues/search?client_id="+client_id+"&client_secret="+client_secret+"" +
+  //    "&v=20130815&near=loc&query=food_item&categoryId="+food_id+"&limit=Lim";
 
 
-    }
+  function init(){
 
-    function searchFourSqrApi(){
-      var foodName = $restNametxt.val();
-      var location = $locationtxt.val();
-      var url = apiUrl
-          .replace("food_item", foodName)
-          .replace("loc", location)
-          .replace("Lim", 10);
-      //alert(url)
+    $restNametxt = $("#restNametxt");
+    $locationtxt = $("#locationtxt");
+    $searchRestbtn = $("#searchRestbtn");
 
-      // Ajax call on success passes the response to callback function i.e. to success parameter function
+    $searchRestbtn.click(searchYelpApi);
+
+    function searchYelpApi() {
+      var terms = $restNametxt.val();
+      var near = $locationtxt.val();
+      var limit = 2;
+      var category_filter = "restaurants";
+      var accessor = {
+        consumerSecret: auth.consumerSecret,
+        tokenSecret: auth.accessTokenSecret
+      };
+      parameters = [];
+      parameters.push(['term', terms]);
+      parameters.push(['location', near]);
+      parameters.push(['limit', 2]);
+      parameters.push(['category_filter', 'restaurants']);
+      parameters.push(['callback', 'cb']);
+      parameters.push(['oauth_consumer_key', auth.consumerKey]);
+      parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+      parameters.push(['oauth_token', auth.accessToken]);
+      parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+      var message = {
+        'action': 'http://api.yelp.com/v2/search',
+        'method': 'GET',
+        'parameters': parameters
+      };
+      OAuth.setTimestampAndNonce(message);
+      OAuth.SignatureMethod.sign(message, accessor);
+      var parameterMap = OAuth.getParameterMap(message.parameters);
+      parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+      //console.log(parameterMap);
       $.ajax({
-          url : url,
-          success : renderRestList
-          //success : function (response){
-          //  console.log(response);
-          //}
-
+        'url': message.action,
+        'data': parameterMap,
+        'cache': true,
+        'dataType': 'jsonp',
+        'jsonpCallback': 'cb',
+        'success': renderRestList
       });
 
     }
-
-    function renderRestList(response){
+    function renderRestList(response, textStats, XMLHttpRequest){
       console.log(response);
+      console.log(textStats); //prints success
     }
+  }
+  //    //alert("hello from foursquare api")
+  //  $restNametxt = $("#restNametxt");
+  //  $locationtxt = $("#locationtxt");
+  //  $searchRestbtn = $("#searchRestbtn");
+  //
+  //  $searchRestbtn.click(searchFourSqrApi);
+  //
+  //
+  //}
+  //
+  //function searchFourSqrApi(){
+  //  var foodName = $restNametxt.val();
+  //  var location = $locationtxt.val();
+  //  var url = apiUrl
+  //      .replace("food_item", foodName)
+  //      .replace("loc", location)
+  //      .replace("Lim", 10);
+  //  //alert(url)
+  //
+  //  // Ajax call on success passes the response to callback function i.e. to success parameter function
+  //  $.ajax({
+  //      url : url,
+  //      success : renderRestList
+  //      //success : function (response){
+  //      //  console.log(response);
+  //      //}
+  //
+  //  });
+  //
+  //}
+  //
+  //function renderRestList(response){
+  //  console.log(response);
+  //}
 
 })();
