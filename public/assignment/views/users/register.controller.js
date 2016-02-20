53b5f14a5ff0424877bf0ff95eeccb1f4f6f9d94
Sanjiv Kumar <skumar@basistech.com>
2016-02-19 21:44:41 -0500

    implemented user registration controller using User Service

--- public/assignment/views/users/register.controller.js
+++ public/assignment/views/users/register.controller.js
@@ -1,3 +1,30 @@
-/**
- * Created by skumar on 2/13/16.
- */
+"use strict";
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("RegisterController", RegisterController);
+
+    function RegisterController($scope, $rootScope, UserService) {
+
+        // Event handler declaration
+        $scope.register = register;
+
+        //Implementation of event handler
+        function register(username, password, vpassword, emailId) {
+            $rootScope.uname = username;
+            $rootScope.pswd = password;
+            $rootScope.vpswd = vpassword;
+            $rootScope.email = emailId;
+
+            var userObj = {"username" : $rootScope.uname, "password" : $rootScope.pswd,
+                "verify password" : $rootScope.vpswd, "email" : $rootScope.email}
+
+
+            UserService.createUser(
+                userObj,
+                function(user){
+                  console.log(user);
+                })
+        }
+    }
+})();
