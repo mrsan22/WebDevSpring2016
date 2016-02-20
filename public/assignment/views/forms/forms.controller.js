4ccb825e604f090e66e516e668552a5443304c9c
Sanjiv Kumar <skumar@basistech.com>
2016-02-20 18:28:46 -0500

    Worked on Form service, Form controller and Form view

--- public/assignment/views/forms/forms.controller.js
+++ public/assignment/views/forms/forms.controller.js
@@ -1,3 +1,40 @@
-/**
- * Created by skumar on 2/13/16.
- */
+"use strict";
+(function () {
+    angular
+        .module("FormBuilderApp")
+        .controller("FormController", FormController);
+
+    function FormController($scope, $rootScope, FormService){
+        var userId;
+         userId = $rootScope.user._id;
+        FormService.findAllFormsForUser(
+            userId,
+            function(form){
+                $scope.forms = form;
+                console.log($scope.forms);
+            }
+        );
+        //Event handler declaration
+        $scope.addForm = addForm;
+        $scope.updateForm = updateForm;
+        //$scope.deleteForm = deleteForm;
+        //$scope.selectForm = selectForm;
+
+        //Implementation of event handler
+        function addForm(formObj){
+            FormService.createFormForUser(
+                userId,
+                formObj,
+                function(form){
+                    console.log(form);
+                    //$scope.forms = form;
+                }
+            )
+
+        }
+
+        function updateForm(formObj){
+
+        }
+    }
+})();
