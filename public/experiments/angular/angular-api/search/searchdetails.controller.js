f6b13fb97f520c8715c7e750f8ae6925bfe2c22c
Sanjiv Kumar <skumar@basistech.com>
2016-03-08 16:24:42 -0500

    Added functionality to submit reviews along with ratings in searchdetails page

--- public/experiments/angular/angular-api/search/searchdetails.controller.js
+++ public/experiments/angular/angular-api/search/searchdetails.controller.js
@@ -6,6 +6,7 @@
 
     function DetailController($scope, $routeParams, YelpService, $interval){
         $scope.restId = $routeParams.restId;
+        $scope.addReview = addReview;
 
         function init(){
             $scope.defaultRating = [{
@@ -20,56 +21,67 @@
             }];
 
             $scope.ratings = [{
-                title : 'Rating 1',
-                description : 'I\'m not editable but update myself with a timer...',
-                rating : 3.5,
-                basedOn : 5,
-                starsCount : 5,
-                iconClass : 'fa fa-mortar-board',
-                editableRating : false,
-                showGrade : true
-            }, {
                 title : 'Rating 2',
-                description : 'I\'m not editable...',
+                description : 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque' +
+                ' ante sollicitudin commodo.',
                 rating : 1,
                 basedOn : 5,
                 starsCount : 5,
                 iconClass : 'fa fa-star',
                 editableRating : false,
-                showGrade : true
+                showGrade : false
             }, {
-                title : 'Rating 3',
-                description : 'I\'m editable...',
-                rating : 0,
-                basedOn : 5,
-                starsCount : 10,
-                iconClass : 'fa fa-star',
-                editableRating : true,
-                showGrade : true
-            }, {
-                title : 'Rating 4',
-                description : 'I\'m editable with a weird icon...',
-                rating : 2.5,
+                title : 'Rating 2',
+                description : 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ' +
+                'ante sollicitudin commodo.',
+                rating : 2,
                 basedOn : 5,
                 starsCount : 5,
-                iconClass : 'fa fa-send',
-                editableRating : true,
-                showGrade : true
-            }, {
-                title : 'Rating 5',
-                description : 'I\'m editable and a lot larger than my neighbours...',
-                rating : 33,
-                basedOn : 100,
-                starsCount : 20,
                 iconClass : 'fa fa-star',
-                editableRating : true,
-                showGrade : true
+                editableRating : false,
+                showGrade : false
+            }, {
+                title : 'Rating 2',
+                description : 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque' +
+                ' ante sollicitudin commodo.',
+                rating : 3.5,
+                basedOn : 5,
+                starsCount : 5,
+                iconClass : 'fa fa-star',
+                editableRating : false,
+                showGrade : false
+            }, {
+                title : 'Rating 2',
+                description : 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque' +
+                ' ante sollicitudin commodo.',
+                rating : 5,
+                basedOn : 5,
+                starsCount : 5,
+                iconClass : 'fa fa-star',
+                editableRating : false,
+                showGrade : false
             }];
             //$interval(function() {
             //    $scope.ratings[0].rating = Math.round(Math.random()*5*100) / 100;
             //}, 1000);
+
         }
         init();
+
+        function addReview(rating, review){
+            console.log(rating);
+            console.log(review);
+            $scope.ratings.unshift({
+                title : 'Rating',
+                description : review,
+                rating : rating,
+                basedOn : 5,
+                starsCount : 5,
+                iconClass : 'fa fa-star',
+                editableRating : false,
+                showGrade : false
+            });
+        }
 
         YelpService.findRestDetailsbyId(
             $scope.restId,
