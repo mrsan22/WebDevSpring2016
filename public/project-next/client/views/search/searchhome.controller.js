97270600f3b71b03e30e2b8b3546e1d0c34590dd
Sanjiv Kumar <skumar@basistech.com>
2016-04-06 11:45:47 -0400

    Added autocomplete and getcurrent location funcitonality in search home page

--- public/project-next/client/views/search/searchhome.controller.js
+++ public/project-next/client/views/search/searchhome.controller.js
@@ -4,11 +4,14 @@
         .module("Eat'n'Review")
         .controller("SearchHomeController", SearchHomeController);
 
-    function SearchHomeController($scope, FourSquareService, YelpService, $location){
+    function SearchHomeController($scope, FourSquareService, YelpService, $location, GoogleService){
         var vm = this;
 
         vm.search = search;
         vm.callSearch = callSearch;
+        vm.getLocation = getLocation;
+        vm.showPosition = showPosition;
+        vm.showError = showError;
 
         function init(){
             $('#carousel').carousel({
@@ -22,8 +25,6 @@
                     var imageurl_lst = [];
                     var restNames = [];
                     var ids = [];
-                    //console.log(response);
-                    //console.log(response.response.groups[0].items);
                     var popularRestLst = response.response.groups[0].items;
                     for (var i in popularRestLst){
                         //console.log(popularRestLst[i].venue.photos.groups[0].items[0]);
@@ -63,15 +64,71 @@
         }
 
         function callSearch(restname, location){
-            YelpService.findRestbyNameLocation(
-                restname,
-                location,
-                function(response){
-                    vm.data = response;
-                    vm.$location.url('/search/restname='+restname+'&location='+location);
-                    $scope.$apply();
-                }
-            )
+            if(typeof location != 'string') {
+                YelpService.findRestbyNameLocation(
+                    restname,
+                    location.formatted_address,
+                    function (response) {
+                        vm.data = response;
+                        vm.$location.url('/search/restname=' + restname + '&location=' + location.formatted_address);
+                        $scope.$apply();
+                    }
+                )
+            }
+            else{
+                YelpService.findRestbyNameLocation(
+                    restname,
+                    location,
+                    function (response) {
+                        vm.data = response;
+                        vm.$location.url('/search/restname=' + restname + '&location=' + location);
+                        $scope.$apply();
+                    }
+                )
+            }
+        }
+
+
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition, showError);
+            } else {
+                console.log("Geolocation is not supported by this browser.");
+
+            }
+        }
+
+        function showPosition(position){
+            console.log("Latitude:" + position.coords.latitude+"<br>Longitude: " + position.coords.longitude);
+            var latlon = position.coords.latitude + "," + position.coords.longitude;
+            GoogleService
+                .findAddressByLatLong(latlon)
+                .then(function (place) {
+                        console.log(place);
+                        console.log(place.data.results[0].formatted_address);
+                        vm.loc = place.data.results[0].formatted_address;
+                    },
+                    function (error) {
+                        console.log("Failed to retrieve place name, given the lat&long", error);
+                    })
+
+        }
+
+        function showError(error) {
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    console.log("User denied the request for Geolocation.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    console.log("Location information is unavailable.");
+                    break;
+                case error.TIMEOUT:
+                    console.log("The request to get user location timed out.");
+                    break;
+                case error.UNKNOWN_ERROR:
+                    console.log("An unknown error occurred.");
+                    break;
+            }
         }
 
 
