b0874a1850b940ec0f2185fe66f7434a5cd4e86e
Sanjiv Kumar <skumar@basistech.com>
2016-04-16 16:35:50 -0400

    Worked on improving UI for profile

--- public/project-next/client/views/users/likes.controller.js
+++ public/project-next/client/views/users/likes.controller.js
@@ -8,7 +8,9 @@
         vm.toggleMenu = toggleMenu;
         vm.likeRest = likeRest;
         vm.undolikeRest=undolikeRest;
-        vm.$location = $location
+        vm.$location = $location;
+        vm.followUser = followUser;
+        vm.unFollowUser = unFollowUser;
 
         function init(){
             vm.userId = $routeParams.userId;
@@ -19,6 +21,7 @@
                         console.log(response.data);
                         vm.currentUser = response.data;
                         getLikesforUser();
+                        isFollowed();
                     },
                     function (error){
                         console.log(error.statusText)
@@ -94,6 +97,49 @@
                     }
                 }, function (error) {
                     console.log("Error in removing like for a Restaurant", error.statusText);
+                })
+        }
+
+        function followUser(userId){
+            console.log(userId);
+            UserService
+                .followUser(userId, vm.currentUser._id)
+                .then(function (response) {
+                    console.log(response);
+                    if(response.status == 200){
+                        vm.isfollowed = true;
+                    }
+                }, function (error) {
+                    console.log("Error in following a user", error.statusText);
+                })
+        }
+
+        //Is user(whose profile currently loggedin user visits) followedby currently loggedin user.
+        function isFollowed(){
+            UserService
+                .isFollowed(vm.userId, vm.currentUser._id)
+                .then(function (response) {
+                    if(response.data){
+                        vm.isfollowed = true;
+                    }
+                    else{
+                        vm.isfollowed = false;
+                    }
+                }, function (error) {
+                    console.log("Error in retrieving restid from likes Array of current User", error.statusText);
+                })
+        }
+
+        function unFollowUser(userId){
+            UserService
+                .unFollowUser(userId, vm.currentUser._id)
+                .then(function (response) {
+                    console.log(response);
+                    if(response.status == 200 && response.data.nModified == 1){
+                        vm.isfollowed = false;
+                    }
+                }, function (error) {
+                    console.log("Error in unFollowing a user", error.statusText);
                 })
         }
 
